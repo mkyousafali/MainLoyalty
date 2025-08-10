856640e38bfcb5fb1f6390db1daf8af234fb6a7b
mkyousafali <mk.yousafali@gmail.com>
2025-08-10 17:20:34 +0300

    Simplify svelte.config.js to help Vercel detection

--- frontend/svelte/svelte.config.js
+++ frontend/svelte/svelte.config.js
@@ -1,16 +1,11 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			// Vercel adapter options
-			runtime: 'nodejs18.x'
-		})
+		adapter: adapter()
 	}
 };
 
